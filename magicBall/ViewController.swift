1557c09138f1e267d1718c02aedf45ee8197d857
Ilya Pogozhev <ilya_pogozhev29@mail.ru>
2023-07-15 21:09:46 +0600

    add 2 view

--- magicBall/ViewController.swift
+++ magicBall/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    let viewBall1: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.layer.cornerRadius = 150
+        view.layer.borderColor = UIColor.white.cgColor
+        view.layer.borderWidth = 1
+        view.snp.makeConstraints {
+            $0.width.equalTo(300)
+            $0.height.equalTo(300)
+        }
+        return view
+    }()
+    let viewBall2: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        view.layer.cornerRadius = 75
+        view.snp.makeConstraints {
+            $0.width.equalTo(150)
+            $0.height.equalTo(150)
+        }
+        return view
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupScene()
+        makeContraints()
     }
-
-
+}
+private extension ViewController {
+    func setupScene() {
+        view.backgroundColor = .purple
+        view.addSubview(viewBall1)
+        viewBall1.addSubview(viewBall2)
+    }
+    func makeContraints() {
+        viewBall1.snp.makeConstraints {
+            $0.centerX.equalTo(view)
+            $0.centerY.equalTo(view).offset(100)
+        }
+        viewBall2.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+        }
+    }
 }
 
